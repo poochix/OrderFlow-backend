9f9c9be0dc728503d652426aba4db99c2d91b4b9
Hritik <hritikdubey805@gmail.com>
2026-09-05 22:14:53 +0530

    ...

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(
   {
-    origin: 'http://localhost:5173',   //frontend URL
+    origin: process.env.CLIENT_URL,   //frontend URL
     credentials: true,                 // Allows cookies and Headers to pass through
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
